7950a333d7cedfd3aa44e61996920a10bbef3aa3
itWithGeorge <107503615+itWithGeorge@users.noreply.github.com>
2025-03-17 09:24:54 +0100

    Delete api_client.dart

--- api_client.dart
+++
@@ -1,6 +0,0 @@
-import 'package:simu_bank_messages/messages.dart' as msg;
-
-abstract class ApiClient {
-  Future<msg.LoginResponse> login(msg.LoginRequest requestParams);
-  Future<msg.TransactionListResponse>load({required msg.TransactionListRequest requestParams});
-}
